24f2f86d5bf8554823bd93dda07ae28663cb8eb7
GajendraLV <gajendralv.xworks@gmail.com>
2025-10-06 11:21:55 +0530

    overiding

---
+++ overriding polymorphism/Dog.java
@@ -0,0 +1,8 @@
+class Dog extends Animal {
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+	public void frequency(){
+		System.out.println("animal makes a frequency");
+}
+}
---
+++ overriding polymorphism/DogRunner.java
@@ -0,0 +1,7 @@
+class DogRunner{
+public static void main(String [] args){
+	Dog dog=new Dog();
+	dog.sound();
+	dog.frequency();
+}
+}
