7e58d93825b8472fdeccbacf84bb49adaef38e3e
GajendraLV <gajendralv.xworks@gmail.com>
2025-11-17 23:08:37 +0530

    Exception

---
+++ Revision/src/com/Revision/external/ProblemRunner.java
@@ -0,0 +1,46 @@
+package com.Revision.external;
+
+import com.Revision.internal.Problem;
+
+public class ProblemRunner {
+    public static void main(String[] args){
+     Problem problem=new Problem();
+     problem.ArithmeticException();
+     problem.AlreadyBoundException();
+     problem.ArrayStoreException();
+     problem.BufferOverflowException();
+     problem.BufferUnderflowException();
+     problem.CannotRedoException();
+     problem.CannotUndoException();
+     problem.ClassCastException();
+     problem.CMMException();
+     problem.ConcurrentModificationException();
+     problem.DateTimeException();
+     problem.EmptyStackException();
+     problem.FileSystemAlreadyExistsException();
+     problem.FileSystemNotFoundException();
+     problem.IllegalArgumentException();
+     problem.IllegalMonitorStateException();
+     problem.IllegalPathStateException();
+     problem.IllegalStateException();
+     problem.IllformedLocaleException();
+     problem.ImagingOpException();
+     problem.IndexOutOfBoundsException();
+     problem.JMRuntimeException();
+     problem.MalformedParameterizedTypeException();
+     problem.MalformedParametersException();
+     problem.NegativeArraySizeException();
+     problem.NoSuchElementException();
+     problem.NoSuchMechanismException();
+     problem.NullPointerException();
+     problem.ProviderException();
+     problem.ProviderNotFoundException();
+     problem.RejectedExecutionException();
+     problem.SecurityException();
+     problem.UnmodifiableSetException();
+     problem.UnsupportedOperationException();
+     problem.WrongMethodTypeException();
+
+
+    }
+}
---
+++ Revision/src/com/Revision/internal/Problem.java
@@ -0,0 +1,165 @@
+package com.Revision.internal;
+
+import javax.management.JMRuntimeException;
+import javax.print.attribute.UnmodifiableSetException;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.xml.crypto.NoSuchMechanismException;
+import java.awt.color.CMMException;
+import java.awt.geom.IllegalPathStateException;
+import java.awt.image.ImagingOpException;
+import java.lang.invoke.WrongMethodTypeException;
+import java.lang.reflect.MalformedParameterizedTypeException;
+import java.lang.reflect.MalformedParametersException;
+import java.nio.BufferOverflowException;
+import java.nio.BufferUnderflowException;
+import java.nio.channels.AlreadyBoundException;
+import java.nio.file.FileSystemAlreadyExistsException;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.ProviderNotFoundException;
+import java.security.ProviderException;
+import java.time.DateTimeException;
+import java.util.*;
+import java.util.concurrent.RejectedExecutionException;
+
+public class Problem {
+    public void ArithmeticException() {
+        throw new ArithmeticException();
+    }
+
+    public void AlreadyBoundException() {
+        throw new AlreadyBoundException();
+    }
+
+    public void ArrayStoreException() {
+        throw new ArrayStoreException();
+    }
+
+    public void BufferOverflowException() {
+        throw new BufferOverflowException();
+    }
+
+    public void BufferUnderflowException() {
+        throw new BufferUnderflowException();
+    }
+
+    public void CannotRedoException() {
+        throw new CannotRedoException();
+    }
+
+    public void CannotUndoException() {
+        throw new CannotUndoException();
+    }
+
+    public void ClassCastException() {
+        throw new ClassCastException();
+    }
+
+    public void CMMException() {
+        throw new CMMException("");
+    }
+
+    public void ConcurrentModificationException() {
+        throw new ConcurrentModificationException();
+    }
+
+    public void DateTimeException() {
+        throw new DateTimeException("");
+    }
+
+    public void EmptyStackException() {
+        throw new EmptyStackException();
+    }
+
+    public void FileSystemAlreadyExistsException() {
+        throw new FileSystemAlreadyExistsException();
+    }
+
+    public void FileSystemNotFoundException() {
+        throw new FileSystemNotFoundException();
+    }
+
+    public void IllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
+
+    public void IllegalMonitorStateException() {
+        throw new IllegalMonitorStateException();
+    }
+
+    public void IllegalPathStateException() {
+        throw new IllegalPathStateException();
+    }
+
+    public void IllegalStateException() {
+        throw new IllegalStateException();
+    }
+
+    public void IllformedLocaleException() {
+        throw new IllformedLocaleException();
+    }
+
+    public void ImagingOpException() {
+        throw new ImagingOpException();
+    }
+
+    public void IndexOutOfBoundsException() {
+        throw new IndexOutOfBoundsException();
+    }
+
+    public void JMRuntimeException() {
+        throw new JMRuntimeException();
+    }
+
+    public void MalformedParameterizedTypeException() {
+        throw new MalformedParameterizedTypeException();
+    }
+
+    public void MalformedParametersException() {
+        throw new MalformedParametersException();
+    }
+
+    public void NegativeArraySizeException() {
+        throw new NegativeArraySizeException();
+    }
+
+    public void NoSuchElementException() {
+        throw new NoSuchElementException();
+    }
+
+    public void NoSuchMechanismException() {
+        throw new NoSuchMechanismException();
+    }
+
+    public void NullPointerException() {
+        throw new NullPointerException();
+    }
+
+    public void ProviderException() {
+        throw new ProviderException();
+    }
+
+    public void ProviderNotFoundException() {
+        throw new ProviderNotFoundException();
+    }
+
+    public void RejectedExecutionException() {
+        throw new RejectedExecutionException();
+    }
+
+    public void SecurityException() {
+        throw new SecurityException();
+    }
+
+    public void UnmodifiableSetException() {
+        throw new UnmodifiableSetException();
+    }
+
+    public void UnsupportedOperationException() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void WrongMethodTypeException() {
+        throw new WrongMethodTypeException();
+    }
+}
